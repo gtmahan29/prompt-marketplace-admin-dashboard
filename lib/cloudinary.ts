3f575fa783f708a83e710414f1727bc8f4a1bb62
Mahan <gtmahan29@gmail.com>
2024-06-11 20:30:22 +0330

    first commit

---
+++ lib/cloudinary.ts
@@ -0,0 +1,15 @@
+import { v2 as cloudinary } from "cloudinary";
+
+// cloudinary config
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.CLOUD_API_KEY,
+//   api_secret: process.env.CLOUD_API_SECRET,
+// });
+
+export default cloudinary;
